2f40c961e782b79d3b7689d5fd4500b40f1f29ef
Portulak <ilyshk999@gmail.com>
2020-06-21 22:17:49 +0300

    main developing finished

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div class="products" v-if="filteredItems.length !== 0">
-            <product v-for="product of filteredItems" :item="product" :image="image" :key="product.id_product"/>
+            <ul>
+                <product v-for="product of filteredItems" :item="product" :key="product.id_product"/>
+            </ul>
         </div>
         <div v-else>
             <p>К сожалению, по вашему запросу ничего не найдено =(</p>
@@ -14,22 +16,74 @@ import product from './Product'
 export default {
     data() {
         return {
-            image: 'https://placehold.it/200x150',
             products: [],
             filteredItems: []
         }
     },
     methods: {
-        filterItems(str) {
-            const reg = new RegExp(str, 'i')
-            this.filteredItems = this.products.filter(item => reg.test(item.product_name))
+        filterItems(str, arr = 'products') {
+            if (str == '') {
+                this.filteredItems = this.products
+            } else {
+                const num = +str
+                let reg = new RegExp(str, 'i')
+                let filter = this[arr].filter(item => {
+                    if (num === num) {
+                        if (item.grade.length > 2) {
+                            let grade = item.grade.split(';')
+                            grade.forEach((el, i) => {grade[i] = parseInt(el)})
+                            if (grade.indexOf(num) != -1) {
+                                return 1
+                            } else {
+                                return 0
+                            }
+                        } else if (+item.grade == num) {
+                            return 1
+                        } else {
+                            return 0
+                        }
+                    } else if (reg.test(item.genre)) {
+                        return 1
+                    } else if (reg.test(item.subject)) {
+                        return 1
+                    } else { return 0}
+                })
+                this.filteredItems = filter
+            }
         },
     },
     mounted() {
-        this.$parent.getJSON('/api/products')
+        this.$parent.postJSON()
             .then(data => {
-                this.products = data
-                this.filteredItems = data
+                this.products = data.items
+                this.filteredItems = data.items
+                let grades = this.$root.$children[0].$refs.filter.grades,
+                    genres = this.$root.$children[0].$refs.filter.genres,
+                    subjects = this.$root.$children[0].$refs.filter.subjects
+
+                data.items.forEach(el => {
+                    if (genres.indexOf(el.genre) == -1) {
+                        genres.push(el.genre)
+                    }
+                    if (subjects.indexOf(el.subject) == -1) {
+                        subjects.push(el.subject)
+                    }
+                    if (el.grade.length > 2) {
+                        let grade = el.grade.split(';')
+                        grade.forEach(item => {
+                            if (grades.indexOf(item) == -1) {
+                                grades.push(item)
+                            }
+                        })
+                    } else {
+                        if (grades.indexOf(el.grade) == -1) {
+                            grades.push(el.grade)
+                        }
+                    }
+                })
+                grades.sort((a, b) => +a > +b ? 1 : -1)
+                subjects.sort((a, b) => a > b ? 1 : -1)
+                genres.sort((a, b) => a > b ? 1 : -1)
             })
     },
     components: {
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,16 +1,39 @@
 <template>
-    <div class="product-item">
-        <img :src="image" alt="Some img">
-        <div class="desc">
-            <h3>{{item.product_name}}</h3>
-            <p>{{item.price}} $</p>
-            <button class="buy-btn" @click="$root.$refs.cart.addProduct(item)">Купить</button>
+    <li class="product-item">
+        <div class="product-item_figure">
+            <img :src="img_url()+item.courseId" :alt="item.subject">
         </div>
-    </div>
+        <div class="product-item_info">
+            <p class="product-item__title">{{item.subject}}</p>
+            <p class="product-item__grade">{{trueGrade(item.grade)}} класс</p>
+            <p class="product-item__genre">{{item.genre}}</p>    
+            <p class="product-item__meta"><a :href="item.shopUrl">Подробнее</a></p>
+            <p class="product-item__controls">
+                <a href="#" class="product-item_buy-btn" v-if="$root.$children[0].currency == 'rub'">{{item.price}} рублей</a>
+                <a href="#" class="product-item_buy-btn" v-else>{{item.priceBonus}} бонусов</a>
+            </p>
+        </div>
+    </li>
 </template>
 
 <script>
 export default {
-    props: ['image', 'item']
+    props: ['item'],
+    data() {
+        return {
+            base_url: 'https://www.imumk.ru/',
+            img_url: () => this.base_url+'svc/coursecover/'
+        }
+    },
+    methods: {
+        trueGrade(str) {
+            if (str.length <= 2) {
+                return str
+            } else {
+                let grade = str.split(';')
+                return `${grade[0]}-${grade[grade.length-1]}`
+            }
+        }
+    }
 }
 </script>
