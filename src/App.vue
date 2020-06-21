b3b1cebefe3e2a7030139dd6671c49c81ca99184
Portulak <ilyshk999@gmail.com>
2020-06-21 22:20:08 +0300

    added all components imports and methods

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,63 @@
 <template>
   <div id="app">
-    
+    <header>
+      <div class="container">
+        <a href="/" class="logo"><h1>Облако знаний</h1></a>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <h1 class="title">Витрина</h1>
+        <div class="currency">
+          <h4>Выводить стоимость:</h4>
+          <label>
+            <input type="radio" name="currency" value="rub" v-model="currency" checked> в рублях
+          </label>
+          <br>
+          <label>
+            <input type="radio" name="currency" value="bonus" v-model="currency"> в бонусах
+          </label>
+        </div>
+        <div class="filter">
+          <filtered-items ref="filter"></filtered-items>
+        </div>
+        <catalog ref="cata"></catalog>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
+import catalog from './components/Catalog'
+import filter from './components/Filter'
 
 export default {
   name: 'App',
-  components: {}
+  data() {
+    return {
+      currency: 'rub'
+    }
+  },
+  methods: {
+      postJSON() {
+          return fetch('http://krapipl.imumk.ru:8082/api/mobilev1/update', {
+              method: 'POST',
+              headers: {
+                  "Content-Type": "text/html"
+              },
+              body: { "data": "" }
+          })
+              .then(result => result.json())
+              .catch(err => {
+                  this.$root.$refs.error.errorShow = true
+                  console.error(err)
+              })
+      }
+  },
+  components: {
+      catalog,
+      'filtered-items': filter
+  }
 }
 </script>
 
